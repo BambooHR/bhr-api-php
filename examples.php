5076bf1a4ed1fa4dd12c4b289db3b4ecb12c24b6
jlesueur@bamboohr.com <jlesueur@bamboohr.com>
2013-10-21 14:17:30 -0600

    Initial public repository Changes from 0.9:         - Classes are now namespaced under BambooHR\API.         - Increased API coverage:                 uploadEmployeeFile()                 updateTimeOffRequest()                 uploadCompanyFile()                 listEmployeeFiles()                 listCompanyFiles()                 addEmployeeFileCategory()                 addCompanyFileCategory()                 updateEmployeeFile()                 updateCompanyFile()                 downloadEmployeeFile()                 importEmployees()                 getDirectory()                 downloadEmployeePhoto()                 getChangedEmployeeTable()         - Some convenience functions:                 API::login()                 HTTPResponse::isError()                 HTTPResponse::getErrorMessage()                 HTTPResponse::getContent()         - improved phpdoc comments for all functions.         - usage example and example import xml file         - Can be used as a composer package.

---
+++ examples.php
@@ -0,0 +1,45 @@
+<?php
+
+include "BambooHR/API/API.php";
+use \BambooHR\API\API as BHR;
+
+//this can be either your subdomain, or the full domain, so either "test", or "test.bamboohr.com"
+$api = new BHR('<your domain>');
+//Users can create an api key in bamboohr, see http://www.bamboohr.com/api/documentation/#authentication for more details.
+$api->setSecretKey('<your key>');
+//or if you have contacted BambooHR to get an application key, you can allow users to login with their usernames and passwords. You should not store usernames and passwords, but store keys instead.
+$authenticated = $api->login('<your application key>', '<username>', '<password>');
+if(!$authenticated) {
+	trigger_error("Login Failure");
+}
+
+//It's a good idea to take a look at the fields that are available in your account
+$response = $api->getFields();
+//$response is an object representing the HTTP response. Errors can be detected by testing the statusCode:
+if($response->statusCode < 200 || $response->statusCode > 299) {
+	trigger_error("Login Failure");
+}
+//or by using $response->isError()
+if($response->isError()) {
+	trigger_error("Login Failure");
+}
+//APIs return in XML format by default. The getContent() function will return a
+//SimpleXMLElement object of the root XML element in the response. If JSON is
+//returned, it will return a stdclass object.
+$xml = $response->getContent();
+foreach($xml->field as $field ){
+	echo "Field: " . $field;
+}
+
+//tables can either be a list of items (like contacts), or a historical record (like job history), but can be accessed in similar ways.
+$response = $api->getTables();
+//This will allow you to view valid values for "list" fields.
+$response = $api->getLists();
+
+$fields = array('lastName', 'firstName', 'employeeNumber');
+$response = $api->getCustomReport('xml',$fields);
+if($response->isError()){
+	//for many errors, Bamboo will add a header "X-BambooHR-Error-Messsage" that describes the error. These messages are meant for debugging purposes.
+	trigger_error("Problem with custom report: " . $response->getErrorMessage());
+}
+
