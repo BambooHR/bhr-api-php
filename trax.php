2dd5471ea5e708987dc1526da776f57040021101
mhuff <mhuff@bamboohr.com>
2022-03-28 15:24:06 -0600

    SPN 698
    LICENSE:
    trax.php:
    What: Added and updated BSD3 License
    Why: To make sure our license is accurate, updated, and what we would like for the SDK.

--- trax.php
+++ trax.php
@@ -2,8 +2,8 @@
 
 /*
 
-Copyright (c) 2011, Bamboo HR LLC
-All rights reserved.
+Copyright (c) 2022 Bamboo HR LLC. All Rights Reserved.
+BambooHR® is a registered trademark of Bamboo HR LLC
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
@@ -15,10 +15,9 @@ are permitted provided that the following conditions are met:
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
-* Neither the name of Bamboo HR nor the names of its contributors may be used
-  to endorse or promote products derived from this software without specific
-  prior written permission.
-
+* Neither the name of the copyright holder nor the names of its contributors
+  may be used to endorse or promote products derived from this software without
+  specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
